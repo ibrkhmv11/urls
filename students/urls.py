b6a0229208215443b172219104fb063d2408e728
Ibragimov Norbek <ibragimovnorbek66@gmail.com>
2026-07-01 18:38:03 +0500

    dasiashf

--- students/urls.py
+++ students/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import ismi , yoshi , malumoti
 
 urlpatterns = [
-    path('ismlar', ismi ),
+    path('ismlar', ismi),
     path('yoshi', yoshi),
     path('malumoti', malumoti),
-
 ]
