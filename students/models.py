44990a663d36eaf963601e071cd1bb329f17b798
Ibragimov Norbek <ibragimovnorbek66@gmail.com>
2026-07-01 18:41:31 +0500

    cihdhc

--- students/models.py
+++ students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
